305ec6e53c922f59ce841a1151027696ad36ab63
ku91l <kunal91@outlook.com>
2014-08-23 12:31:52 +0530

    File Contains the Program for caching inverse of a matrix
    A dummy matrix was initialized to make the comptations

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+seq1=seq(1:6)
+y<-matrix(seq1,2)
 makeCacheMatrix <- function(x = matrix()) {
+i <- NULL
+set <- function(y){
+x <<- y
+i <<- NULL}
+get <- function() x
+setInverse <- function(solve) i<<-solve(x)
+getInverse <- function() i
+list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+
 
 }
 
@@ -11,5 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+i<-x$getInverse()
+if(!is.NULL(i))
+{message("Getting Cached")
+return(i)
+}
+data<-x$get()
+m<-(data,...)
+x$setInverse(i)
+i}
         ## Return a matrix that is the inverse of 'x'
 }
